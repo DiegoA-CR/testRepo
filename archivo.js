0d506f64ce3ad6e209607577edac68ebff9a9614
DiegoA-CR <diego.a.calvarior24@gmail.com>
2026-05-18 21:19:30 -0600

    se agrega barra de navegacion sencilla

---
+++ archivo.js
@@ -0,0 +1,21 @@
+
+let Menu = document.getElementById('Menu');
+
+function mostrar(){
+	if(Menu.style.display == "none"){
+
+		Menu.style.display = "block";
+	} else {
+
+		Menu.style.display = "none";
+	}
+}
+
+{/*
+
+function mostrar() {
+    Menu.style.display =
+        Menu.style.display == "none" ? "block" : "none";
+}
+*/}
+
